c9b20b5196c45249ae91fa9526b1c9c616785883
nrsumithra <sumimars24@gmail.com>
2021-11-17 02:06:02 +0530

    commit

--- HOUSE PRICE/House Price Predict_Model/HousePrice_Flask.py
+++ HOUSE PRICE/House Price Predict_Model/HousePrice_Flask.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app=Flask(__name__)
 
 @app.route('/')
 def houseprice():
-    return render_template('index.html')
+    return render_template('form.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    BHK = request.form.get('BHK')   
+    Bath = request.form.get('Bath') 
+    final_sqft = request.form.get('Sqft') 
+    location = request.form.get('Location') 
+
+    print(BHK, Bath, final_sqft, location)
+    return 'Form is submitted'
 
 app.run(debug=True)
